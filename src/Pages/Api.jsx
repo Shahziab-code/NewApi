5dcfd2a134d2b31d037067682c578fc202774fba
Shahziab-code <Shahzaib.soomro123@gmail.com>
2026-01-02 18:08:07 +0500

    Api Fetched

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,42 +1,44 @@
 import { useEffect, useState } from "react";
-import '../Pages/Api.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import "../Pages/Api.css";
 const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 
 /**
- * @typedef {Object} Post 
- * @property {number} id 
- * @property {string} title 
+ * @typedef {Object} Post
+ * @property {number} id
+ * @property {string} title
  */
 
 const Api = () => {
   const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const Fetch = async () => {
-        setIsLoading(true)
-        if (posts) {
-        const savePosts =  localStorage.getItem(posts.title)
-        setPosts(savePosts)
-        } else {
-         try {
-        const res = await fetch(`${BASE_URL}`);
-        const posts = await res.json();
-        setPosts(posts);
-        localStorage.setItem('posts', JSON.stringify(posts))
-        console.log(posts);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false)
-      }
+    const savePosts = JSON.parse(localStorage.getItem("posts") || "[]");
+    if (savePosts.length > 0) {
+      setPosts(savePosts);
+      console.log("Data stored in local storage successfully", savePosts);
+    } else {
+      const Fetch = async () => {
+        setIsLoading(true);
+        try {
+          const res = await fetch(`${BASE_URL}`);
+          const posts = await res.json();
+          setPosts(posts);
+          console.log(posts);
+        } catch (error) {
+          console.log("Error: ", error);
+        } finally {
+          setIsLoading(false);
         }
-    };
-    Fetch();
-  }, [posts]);
+      };
+      Fetch();
+    }
+  }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
@@ -44,9 +46,15 @@ const Api = () => {
       <h1>Fetching Api</h1>
       <div className="container">
         <ul>
-            {posts.map((post) => (
-                <li key={post.id}>{post.title}</li>
-             ))} 
+          {posts.map((post) => (
+            <div className="postItem" key={post.id}>
+              <li key={post.id}>{post.title}</li>
+              <div>
+                <FontAwesomeIcon className="editBtn" icon={faPenToSquare} />
+                <FontAwesomeIcon className="editBtn" icon={faTrash} />
+              </div>
+            </div>
+          ))}
         </ul>
       </div>
     </div>
